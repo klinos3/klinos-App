7e115eb4099bbc96e268cd171e8bda67dbe7defd
klinos3 <info@klinosinsight.com>
2025-08-26 00:48:19 +0100

    usar App.jsx como componente principal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';   // 👈 garante que usa o App.jsx
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
